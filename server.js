bd715e32a1c9e6bd95628e0cb261cb2e9e09dfc6
Takuji Okubo <takuji.okubo@gmail.com>
2020-04-02 23:25:49 +1100

    Update fro server.js

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var express = require("express");
 var PORT = process.env.PORT || 3000;
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'client','build')));
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'client','index.html'));
 });
 
 app.listen(PORT, function() {
